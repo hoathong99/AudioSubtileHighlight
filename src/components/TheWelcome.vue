21fb921d1acc087babe48c18d93d40ebf77cbcf0
Vu Ngoc Anh <hoathong99@gmail.com>
2025-02-26 19:28:24 +0700

    clear up comments

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -9,22 +9,11 @@
       <source :src="audioUrl" type="audio/ogg" />
       Your browser does not support the audio element.
     </audio>
-    <!-- <div class="subtitles">
-      <p v-for="(line, index) in displayDialog.dialog" :key="index" v-html="line.text"></p>
-    </div> -->
+
     <pre>
       <div v-html="currentSubtitle"></div>
     </pre>
-    <!-- <div class="subtitles">
-      
-    </div> -->
-    <!-- Subtitle Display -->
-    <!-- <div class="subtitle" v-if="currentSubtitle">
-      {{ currentSubtitle }}
-    </div> -->
 
-    <!-- Button to Manually Stop Subtitles -->
-    <!-- <button @click="stopSubtitlesManually">Stop Subtitles</button> -->
   </div>
 </template>
 
@@ -59,9 +48,6 @@ export default {
           this.subtitles = JSON.parse(e.target.result).timestamp;
           this.displayDialog = JSON.parse(e.target.result);
           this.audioObject = JSON.parse(e.target.result);
-          // this.audioObject.dialog.forEach(element => {
-          //   this.displaySubString.concat("/n", element.text);
-          // });
           this.displaySubString = this.audioObject.dialog.map(u => u.text).join("<br>");
           console.log(this.displaySubString);
           let pos = this.displaySubString.indexOf("<br>");
@@ -75,66 +61,15 @@ export default {
         reader.readAsText(file);
       }
     },
-    // runSubtitles() {
-    //   if (!this.subtitles.length) return;
-    //   let currentTime = this.$refs.audioPlayer.currentTime * 1000;
-    //   console.log("currentTime ", currentTime);
-    //   // Stop any running subtitles
-    //   this.stopSubtitles();
-
-    //   this.subtitles.forEach((sub) => {
-    //     const [start, duration, word] = sub;
-    //       const showSubtitle = setTimeout(() => {
-    //         this.currentSubtitle = word;
-    //         console.log("Current Subtitle:", sub); // Log subtitle to console
-    //         // Set another timeout to clear the subtitle after duration
-    //         const hideSubtitle = setTimeout(() => {
-    //           this.currentSubtitle = "";
-    //         }, duration);
-
-    //         this.subtitleTimeouts.push(hideSubtitle);
-    //       }, start);
-
-    //       this.subtitleTimeouts.push(showSubtitle);
-
-    //   });
-
-    // },
-
-    // runSubtitles() {
-    //   if (!this.displayDialog.timestamp.length) return;
-    //   let currentTime = this.$refs.audioPlayer.currentTime * 1000;
-    //   console.log("currentTime ", currentTime);
-    //   // Stop any running subtitles
-    //   this.stopSubtitles();
-
-    //   this.displayDialog.timestamp.forEach((sub) => {
-    //     const [start, duration, word, idx, length] = sub;
-    //       const showSubtitle = setTimeout(() => {
-    //         this.currentSubtitle = word;
-    //         console.log("Current Subtitle:", sub); // Log subtitle to console
-    //         // Set another timeout to clear the subtitle after duration
-    //         const hideSubtitle = setTimeout(() => {
-    //           this.currentSubtitle = "";
-    //         }, duration);
-
-    //         this.subtitleTimeouts.push(hideSubtitle);
-    //       }, start);
-
-    //       this.subtitleTimeouts.push(showSubtitle);
-
-    //   });
 
     runSubtitles() {
       this.displayDialog.dialog.forEach(element => {
-        // this.displaySubString.concat("/n", element.text);
+
       });
-      // console.log("currentTime ", this.displaySubString);
       if (!this.displayDialog.timestamp.length) return;
       let currentTime = this.$refs.audioPlayer.currentTime * 1000;
       let currentIdx = 0;
-      // console.log("currentTime ", currentTime);
-      // Stop any running subtitles
+
       this.stopSubtitles();
 
       for (let i = 0; i < this.displayDialog.timestamp.length; i++) {
@@ -146,15 +81,11 @@ export default {
         }
 
         const showSubtitle = setTimeout(() => {
-          // this.currentSubtitle = word;
-          // console.log("Current Subtitle:", word, `(Index: ${idx})`);
-          // Set another timeout to clear the subtitle after duration
+
           const hideSubtitle = setTimeout(() => {
             this.idxTracker = currentIdx.valueOf;
             this.currentSubtitle = "";
-            // console.log("index Tracker:", this.idxTracker);
             this.currentSubtitle = this.highlightText(idx, length);
-            console.log(this.currentSubtitle);
           }, duration);
 
           this.subtitleTimeouts.push(hideSubtitle);
@@ -169,19 +100,10 @@ export default {
       // Clear all scheduled subtitle timeouts
       this.subtitleTimeouts.forEach(clearTimeout);
       this.subtitleTimeouts = [];
-      // this.currentSubtitle = "";
     },
 
-    // highlightText(dialogIdx, startIndex, subStringLength) {
-    //   let currentWorkingLine = this.displayDialog.dialog[dialogIdx].text;
-    //   if (startIndex === null || subStringLength === null) return currentWorkingLine;
-    //   const before = currentWorkingLine.slice(0, startIndex - dialogIdx);
-    //   const highlight = currentWorkingLine.slice(startIndex - dialogIdx, startIndex - dialogIdx + subStringLength);
-    //   const after = currentWorkingLine.slice(startIndex - dialogIdx + subStringLength);
-    //   return `${before}<span class='highlight'>${highlight}</span>${after}`;
-    // },
+   
     highlightText(startIndex, subStringLength) {
-      // let totalBr = 0;
       let flag = this.idxBr;
       flag.forEach(e => {
         if(startIndex > e){
